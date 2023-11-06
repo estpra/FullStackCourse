3e275d4ab3721fad08ba6e174b4699163f2222c7
estpra <estpra2k@gmail.com>
2023-11-06 11:41:19 -0800

    Adding my simon game project and the provided completed version

---
+++ Full Stack Course Assignments/Simon Game Project/index.js
@@ -0,0 +1,9 @@
+//this line of code adds an event listener for a mouse click that return true or false whether the classList of the targeted element(target) contains the word in the parameters, will be useful for the switch statement we may need to add
+$(".btn").click((event)=> {
+    console.log(event.target.classList.contains("green"))
+
+    //This logic will be useful for the highScore variable
+$("span").text("Like DJ Kahlid")
+console.log(Math.floor(Math.random() * 4))
+})
+
