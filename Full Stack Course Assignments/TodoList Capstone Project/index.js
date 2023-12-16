1115865c7ab63359b5ffe2f4cc98200720dcec40
estpra <estpra2k@gmail.com>
2023-12-15 19:39:38 -0800

    Fixed bug where delete request wasnt working and created modify(put) endpoint, also modified name and selector for form element

--- Full Stack Course Assignments/TodoList Capstone Project/index.js
+++ Full Stack Course Assignments/TodoList Capstone Project/index.js
@@ -2,33 +2,71 @@ import express from "express"
 import bodyParser from "body-parser"
 
 //Going to go with an array of objects where the object conatains the "date" attribute to hold the inputted date and the "value" attribute to hold the todo item as the value, this will allow me to use the built in sort method for arrays based on the value for the date attribute in ascending meaning least to greatest
-const todoItems = []
+const todoItems = [
+    //Keep in mind that the id attribute will be an int not a string in my production version, just keeping it as a string for now; when it comes to the production version, I will parse the value of the hidden input field to an int(Got ahead of myself and already turned them to ints before pushing to git lol) 
+    {id: 0, date: "2023-12-14", value: "Test1"},
+    {id: 1, date: "2023-12-18", value: "Test2"},
+    {id: 2, date: "2023-12-16", value: "Test3"},
+    {id: 3, date: "2023-12-15", value: "Test4"}
+]
+//this variable keeps track of the unique id numbers assigned to each todo item by incrementing it by 1 after creating and adding a new todo item to to todoItems array
+//this variable will be used as the value for the hidden input field 
+let idNum = 0
 const app = express()
 const port = 3000
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
+//Thinking about sending the todItems array to the front end in this endpoint
 app.get("/", (req, res)=> {
     res.render("index.ejs")
+    //thinking this is how the final res.render will look like to pass in the todoItems array to the front end; note that we have to send a JS object as a response, hence why I put the todoItems array as the value of the items attribute of the JS object that is being sent over as a response
+    // res.render("index.ejs", {items: todoItems})
+    console.log(todoItems)
 })
 
 app.post("/submit", (req, res)=>{
     console.log(req.body)
     //decided to push the date as the string provided by the input element of type date and not convert it to a Date object, because when you create a new Date object in JS, it prints out the date in the format 2023-12-15T00:00:00.000Z, and you cant use that value as a value for the input element of type date; instead, I converted them to Date objects in the lambda expression for the sort function
-    todoItems.push({date: req.body.date, value: req.body.todoItem})
+    todoItems.push({id: idNum, date: req.body.date, value: req.body.todoItem})
     //this line actually sorts the array by date, the order is ascending meaning least to greatest; if I wanted it to be descending order(greatest to least) just change the order of the lambda experession from a-b to b-a(this works becuase the sort method rules say that if the value of the subtraction in the lambda expression is positive, then the sorting order is b before a, if the value is zero, the sorting order is let them stay in the same place, if the value is negative, then the sorting order is a before b)
     todoItems.sort((a, b)=> new Date(a.date) - new Date(b.date))
     console.log(todoItems)
-    console.log(todoItems[0].date)
+    // console.log(todoItems[0].date)
+    // res.render("index.ejs")
+    idNum++
+    res.redirect("/")
 })
 
-app.delete("/delete", (req, res)=>{
-    let indexOfItemToRemove = todoItems.findIndex(obj => obj.value === req.body.todoItem)
+app.delete("/delete/:id", (req, res)=>{
+    //added the parseInt function in order to parse the value of the hidden input field from a string to an int, did this cuz I decided 
+    const id = parseInt(req.params.id)
+    let indexOfItemToRemove = todoItems.findIndex(obj => obj.id === id)
     if(indexOfItemToRemove !== -1)
     {
         todoItems.splice(indexOfItemToRemove, 1)
     }
+    console.log(todoItems)
+    res.render("index.ejs")
+    //for some reason, res.redirect("/") breaks the response; my theory is that since we're supposed to be on the /delete/:id route and since we use the redirect function to redirect us to the homepage(which is the "/" url), the response sends back a 404 error saying that the DELETE request couldnt be found
+})
+
+app.put("/modify/:id", (req, res)=>{
+    console.log(req.body)
+    console.log(req.params)
+    const id = parseInt(req.params.id)
+    let indexOfItemToUpdate = todoItems.findIndex(obj => obj.id === id)
+    if(indexOfItemToUpdate !== -1)
+    {
+        //note that the value for the value attribute is req.body.item and not req.body.todoItem like it is in the "/" route becuase this req.body comes from the JS object that I sent over from the front end using axios and I'm thinking about using the attribute name "item" rather than "todoItem" to avoid redundancy when sending over the body from the front end using axios 
+        todoItems[indexOfItemToUpdate] = {id: id, date: req.body.date, value: req.body.item}
+    }
+    console.log(todoItems)
+    //Going to see if just sending the status code along with a message as a response is enough or if I need to use res.render to avoid having the page's loading circle load indefinitely
+    //Thinking that I dont need to send the index.ejs file again, that way I can avoid having to render each individual todo item on the front end like how I'm planning
+    res.status(200).send("Updated Successfully")
 })
 
 app.listen(port, (req, res)=> {
--- Full Stack Course Assignments/TodoList Capstone Project/public/js/DOM.js
+++ Full Stack Course Assignments/TodoList Capstone Project/public/js/DOM.js
@@ -5,16 +5,15 @@
 //     console.log(document.querySelector("footer").classList.value)
 // }
 
-document.querySelector("#drk-mode-btn").addEventListener("click", ()=>{
+$("#drk-mode-btn").on("click", () => {
     // document.querySelector("html").toggleAttribute("data-bs-theme")
-    if(document.querySelector("html").getAttribute("data-bs-theme") === "dark")
-    {
+    if (document.querySelector("html").getAttribute("data-bs-theme") === "dark") {
         document.querySelector("html").removeAttribute("data-bs-theme")
         //Added this line of code to be able to remove the light grey background that is left over after changing the theme to dark; since we start with the dark theme(I made this default by starting with the data-bs-theme attribute added to the html element) we remove the class that adds the background color that matched the dark mode background
         $("textarea").removeClass("todo-item")
         $("small>input").removeClass("completion")
     }
-    else{
+    else {
         //if you get here, the theme is light, not dark, so add the dark theme to the whole page
         document.querySelector("html").setAttribute("data-bs-theme", "dark")
         //this line adds the class that applies the dark background back to the todo items in order to match the dark theme
@@ -27,3 +26,36 @@ document.querySelector("#drk-mode-btn").addEventListener("click", ()=>{
 // document.querySelector(".todo-item").addEventListener("keydown", ()=>{
 
 // })
+
+//gave the todo item forms an id to identify them by their id name so I wouldnt have to create the axios code to handle the POST request, cuz if I target the form element, since it prevents its default request(which is post in this case), the form for the create a reminder would not create a post request like it should, instead it would go to the event applied to it which is the axios http requests and then I would have to handle the post reqeust there; so really, did this for convinience 
+$("form[name=\"myForm\"]").on("submit", function (event) {
+    //this line of code prevents the default behavior of a form which is to send a post request and allows the axios code to work properly; without this line, the console.logs on the browser dont pop up for some reason
+    event.preventDefault()
+    //REMEMEBR, in order to view these console.logs, you must go to the console on the browser since this js file is used to manipilate the DOM which is bound to the browser; its why we use node.js to be able to use js outside of the browser and for the backend
+    console.log("Event handler works")
+    console.log(event.originalEvent.submitter.name)
+    console.log("id: " + this.elements.id.value)
+    console.log(event)
+    if (event.originalEvent.submitter.name === "delete") {
+        axios.delete(`/delete/${this.elements.id.value}`)
+            .then((res) => {
+                console.log(res)
+                console.log("Deleted successfully")
+            })
+            .catch((err) => console.error(err))
+    }
+    else if (event.originalEvent.submitter.name === "modify") {
+        console.log("date: " + this.elements.date.value)
+        console.log("todoItem: " + this.elements.todoItem.value)
+        let newData = {
+            date: this.elements.date.value,
+            item: this.elements.todoItem.value
+        }
+        axios.put(`/modify/${this.elements.id.value}`, newData)
+            .then((res) => {
+                console.log(res)
+                console.log("Updated Successfully")
+            })
+            .catch((err) => console.error(err))
+    }
+})
