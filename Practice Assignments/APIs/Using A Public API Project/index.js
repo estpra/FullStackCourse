19367be57a780e63595bd05513aa1dbc358994d8
estpra <estpra2k@gmail.com>
2024-02-01 13:20:49 -0800

    Adding progress on Public API Project to continue on mac

--- Practice Assignments/APIs/Using A Public API Project/index.js
+++ Practice Assignments/APIs/Using A Public API Project/index.js
@@ -6,6 +6,10 @@ const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.get("/", (req, res)=> {
+    res.render("index.ejs")
+})
+
 app.listen(port, (req, res)=> {
     console.log(`Listening on port ${port}`);
 })
